4585a42a8e30d4d142057306008e0e7be96bdb30
unknown <myandriychuk@gmail.com>
2018-02-05 15:09:07 +0200

    Add Person class

--- src/git/Main.java
+++ src/git/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello_Wolrd");
+		
+		Person person = new Person("Marcus", 25);
+		System.out.println(person.toString());
 	}
 	
 }
